7d6b6fcc64899ebf4596f3cf0ce54c2cf03e9925
Paulo Kim <paulo.kim@grupoboticario.com.br>
2022-04-18 11:11:28 -0300

    feat: start building api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-import "reflect-metadata";
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+
+import config from './core/config';
+
+const addMiddlewares = (app: express.Application) => {
+  app.use(cors());
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: false }));
+};
+
+const startServer = () => {
+  const app = express();
+
+  addMiddlewares(app);
+
+  app.listen(config.PORT, () =>
+    console.log(`Listening on PORT ${config.PORT}`)
+  );
+};
+
+startServer();
