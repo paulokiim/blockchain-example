ad613186822583303ec2a50c2cbbd15eaa861d43
Paulo Kim <paulo.kim@grupoboticario.com.br>
2022-04-20 16:57:24 -0300

    refactor: manager name

--- src/controllers/chain.ts
+++ src/controllers/chain.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import chainManager from '../manager/cain';
+import chainManager from '../manager/chain';
 
 const addBlock = (req: Request, res: Response) => {
   const body = req.body;
