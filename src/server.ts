691e666ff0f4e3d38453e2ab7d0bc1cc9ae865b8
Paulo Kim <paulo.kim@grupoboticario.com.br>
2022-04-26 21:07:23 -0300

    build: using multer

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
+import multer from 'multer';
 
 import blockchainRoutes from './routes/blockchain';
 import healthcheckRoutes from './routes/healthcheck';
 
 const addMiddlewares = (app: express.Application) => {
+  app.use(multer().any());
   app.use(cors());
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ extended: false }));
