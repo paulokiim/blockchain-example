0977214aed0d11aba29adf64b9d78046a7635319
Paulo Kim <paulo.kim@grupoboticario.com.br>
2022-04-27 21:18:33 -0300

    feat: add connection to database on start

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+
 import config from './core/config';
 import server from './server';
-
 import chain from './core/chain';
+import { exit } from 'process';
 
 const startBlockchain = () => chain.createBlockchain();
 
-const startServer = () => {
+const startServer = async () => {
   const app = server();
 
   startBlockchain();
+  try {
+    await createConnection(config.DATABASE_CONFIG);
 
-  app.listen(config.PORT, () =>
-    console.log(`Listening to PORT ${config.PORT}`)
-  );
+    app.listen(config.PORT, () =>
+      console.log(`Listening to PORT ${config.PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+    throw new Error('Could not connect to database');
+  }
 };
 
 startServer();
