4c925d30fc65dafabf7a54d748ffb1ae3f559a6a
Paulo Kim <paulo.kim@grupoboticario.com.br>
2022-05-01 15:55:39 -0300

    feat: add jwt auth on chain route

--- src/routes/blockchain.ts
+++ src/routes/blockchain.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import chainController from '../controllers/chain';
+import auth from '../auth';
 
 const router = express.Router();
 
-router.post('/block', chainController.addBlock);
+router.post('/block', auth.checkAuthentication, chainController.addBlock);
 
 export default router;
