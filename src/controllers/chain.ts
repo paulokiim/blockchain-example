dedde1250461b8eb428c9552231ff014457bd3c3
Paulo Kim <paulo.kim@grupoboticario.com.br>
2022-05-04 10:41:15 -0300

    chore: add hash uid on block data

--- src/controllers/chain.ts
+++ src/controllers/chain.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 
+import { createHash } from '../utils/hash';
 import chainManager from '../manager/chain';
 
 const addBlock = (req: Request, res: Response) => {
   const file = req.file as Express.MulterS3.File;
+  const body = req.body;
 
-  const blockData = {
+  const blockData: AddBlockParams = {
     data: {
+      accountHash: createHash(body.uid),
       filename: file.key,
       url: file.location,
     },
